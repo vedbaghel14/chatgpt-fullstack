3db6093e5dd2c48a82c87862a8c2386b88fc2e79
vedbaghel14 <baghelved341@gmail.com>
2026-05-22 23:03:10 +0530

    Add favicon, icons, and index.html for the frontend setup
    - Created a new favicon.svg file for the application icon.
    - Added icons.svg containing various SVG symbols for UI components.
    - Introduced index.html as the main entry point for the frontend, linking the favicon and stylesheets.

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,12 +2,17 @@ const express = require('express')
 const cookieparser = require('cookie-parser')
 const authrouter = require('./routes/auth.router')
 const chatrouter = require('./routes/chat.router')
+const path = require('path')
 
 const app = express()
 app.use(express.json())
 app.use(cookieparser())
 app.use('/api/auth',authrouter)
 app.use('/api/chat',chatrouter)
+app.use(express.static(path.join(__dirname,'public')))
 
+app.get("*name",(req,res)=>{
+    res.sendFile(path.join(__dirname,'../public/index.html'))
+})
 
 module.exports = app
