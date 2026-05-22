1a7d98a118cab5d048c848cd620c5c1ef7412ec6
vedbaghel14 <baghelved341@gmail.com>
2026-05-22 23:24:58 +0530

    Fix path to index.html in catch-all route

--- backend/src/app.js
+++ backend/src/app.js
@@ -12,7 +12,7 @@ app.use('/api/chat',chatrouter)
 app.use(express.static(path.join(__dirname,'public')))
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '...public/index.html'))
+    res.sendFile(path.join(__dirname, '..public/index.html'))
 })
 
 module.exports = app
