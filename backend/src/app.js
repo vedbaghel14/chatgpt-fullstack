976630f78da4ac129e6e96e8f3974a02985682c7
vedbaghel14 <baghelved341@gmail.com>
2026-05-22 23:16:04 +0530

    Fix route handler for serving index.html in catch-all route

--- backend/src/app.js
+++ backend/src/app.js
@@ -11,8 +11,8 @@ app.use('/api/auth',authrouter)
 app.use('/api/chat',chatrouter)
 app.use(express.static(path.join(__dirname,'public')))
 
-app.get("*name",(req,res)=>{
-    res.sendFile(path.join(__dirname,'public','index.html'))
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'))
 })
 
 module.exports = app
