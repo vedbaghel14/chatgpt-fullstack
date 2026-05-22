3c087fef2ea567ef3b5dec9b9a372fcedbe7cd7e
vedbaghel14 <baghelved341@gmail.com>
2026-05-22 23:35:34 +0530

    changed

--- backend/src/app.js
+++ backend/src/app.js
@@ -11,7 +11,7 @@ app.use('/api/auth',authrouter)
 app.use('/api/chat',chatrouter)
 app.use(express.static(path.join(__dirname,'..','public')))
 
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, '..','public', 'index.html'))
 })
 
